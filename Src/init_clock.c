4089e2d40dc0a9ab913d3448bfc0b02cdbd41a06
Nejc Sušnik <nejc.susnik05@gmail.com>
2025-10-22 08:36:34 +0200

    some changes

--- Src/init_clock.c
+++ Src/init_clock.c
@@ -55,9 +55,55 @@ void select_sys_clk(uint8_t clock) {
     *rcc_cfgr = curr;
 }
 
+void set_domain_config() {
+    volatile uint32_t *rcc_dom1_cfgr = (uint32_t *)(RCC_REG + 0x18);
+    volatile uint32_t *rcc_dom2_cfgr = (uint32_t *)(RCC_REG + 0x1C);
+    volatile uint32_t *rcc_dom3_cfgr = (uint32_t *)(RCC_REG + 0x20);
+
+    uint32_t curr = *rcc_dom1_cfgr;
+    curr &= ~0b111101111111; //clear
+    curr |= 0b0000 << 8; //D1CPRE = /1
+    curr |= 0b100 << 4; //D1PPRE = /2
+    curr |= 0b1000; //HPRE = /2
+    *rcc_dom1_cfgr = curr;
+
+    curr = *rcc_dom2_cfgr;
+    curr &= ~0b11101110000; //clear
+    curr |= 0b100 << 8; //D2PPRE2 = /2
+    curr |= 0b100 << 4; //D2PPRE1 = /2
+    *rcc_dom2_cfgr = curr;
+
+    curr = *rcc_dom3_cfgr;
+    curr &= ~0b1110000; //clear
+    curr |= 0b100 << 4; //D3PPRE = /2
+    *rcc_dom3_cfgr = curr;
+}
+
+void configure_pll() {
+    volatile uint32_t *rcc_pllckselr = (uint32_t *)(RCC_REG + 0x28);
+    volatile uint32_t *rcc_pllcfgr = (uint32_t *)(RCC_REG + 0x2C);
+    volatile uint32_t *rcc_cr = (uint32_t *)(RCC_REG);
+
+    uint32_t curr = *rcc_pllckselr;
+    curr &= ~0x03F3F3F3;
+    curr |= 0b10; //PLLSRC
+    curr |= 0b10 << 4; //DIVM1
+    curr |= 0b10 << 12; //DIVM2
+    curr |= 0b100000 << 20; //DIVM3
+    *rcc_pllckselr = curr;
+
+    //wait for PLLs to be disabled
+    *rcc_cr &= ~((1 << 28) | (1 << 26) | (1 << 24));
+    while (*rcc_cr & ((1 << 29) | (1 << 27) | (1 << 25)));
+
+    curr = *rcc_pllcfgr;
+    curr &= ~0x01FF0FFF;
+
+}
+
 void init_clock() {
     //optionally change MCO1 and MCO2
-
+    prepare_flash();
 
     while(1);
 }
